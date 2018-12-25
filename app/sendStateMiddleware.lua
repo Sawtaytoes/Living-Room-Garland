c39c7bbd52743d5ef7369f2209a9ef434722803d
Kevin Ghadyani <Saturn2888@gmail.com>
2018-12-25 02:15:50 -0600

    Added proper HTTP response headers to sendStateMiddleware

--- app/sendStateMiddleware.lua
+++ app/sendStateMiddleware.lua
@@ -1,11 +1,28 @@
 local actions = loadfile('actions.lc')()
 local libraries = loadfile('libraries.lc')()
 
+local responseHeaders = {
+	'HTTP/1.1 200 OK',
+	'Server: NodeMCU on ESP8266',
+	'Content-Type: application/json',
+	'\r\n',
+}
+
 local sendStateMiddleware = (
 	function(store)
 		return function()
 			return function(action)
 				if action.type == actions.SEND_STATE then
+					for i, responseHeader in ipairs(responseHeaders) do
+						action
+						.socket
+						:send(responseHeader)
+
+						action
+						.socket
+						:send('\r\n')
+					end
+
 					action
 					.socket
 					:send(
